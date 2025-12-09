1f08500a4478d0993dab4fb558ae8d107984549e
giulio_pastore <giulio.pastore@adesso.it>
2025-12-09 12:48:00 +0100

    Added prod to deploy.yml

--- src/main/java/com/giuliopastore/BankApp/BankAppApplication.java
+++ src/main/java/com/giuliopastore/BankApp/BankAppApplication.java
@@ -1,13 +1,20 @@
 package com.giuliopastore.BankApp;
 
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 
 @SpringBootApplication
 public class BankAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankAppApplication.class, args);
+		SpringApplication application = new SpringApplication(BankAppApplication.class);
+		ConfigurableEnvironment environment = new StandardEnvironment();
+		DotenvPropertySource.addToEnvironment(environment);
+		application.setEnvironment(environment);
+		application.run(args);
 	}
 
 }
