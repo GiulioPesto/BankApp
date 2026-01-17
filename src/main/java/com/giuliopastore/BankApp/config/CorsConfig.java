82502af556cf0d3fe380fd9cb9bbf95b751ff299
giulio_pastore <giulio.pastore@adesso.it>
2026-01-17 15:34:09 +0100

    FIxed CORS configuration

--- src/main/java/com/giuliopastore/BankApp/config/CorsConfig.java
+++ src/main/java/com/giuliopastore/BankApp/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost:5173", "https://pestobank.wasd.solutions", "https://www.pestobank.wasd.solutions")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
